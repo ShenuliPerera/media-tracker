bb15a16aeed2f46037f59bbf5c1a7f96b42823a4
ShenuliPerera <keshiyaperera1@gmail.com>
2026-06-29 13:55:32 +0100

    Add menu function

--- main.py
+++ main.py
@@ -6,5 +6,14 @@ def show_menu():
     print("3. Add a Book")
     print("4. View my list")
     print("5. Quit")
+def main():
+    while True:
+        show_menu()
+        choice = input("\nEnter your choice")
+
+        if choice == "1":
+            print ("You choose: Add a movie")
+        
+
 
 show_menu()
