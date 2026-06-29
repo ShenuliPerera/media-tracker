ce3665a94d43ba8c47265b4c7ef83c586b27bc40
ShenuliPerera <keshiyaperera1@gmail.com>
2026-06-29 13:50:38 +0100

    Add menu function

---
+++ main.py
@@ -0,0 +1,10 @@
+def show_menu():
+    print("Welcome to Media Tracker!")
+    print("---------------------------")
+    print("1. Add a movie")
+    print("2. Add a TV show")
+    print("3. Add a Book")
+    print("4. View my list")
+    print("5. Quit")
+
+show_menu()
