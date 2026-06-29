a0a5f91815654c4da6c9f5c0605620817e1e76eb
ShenuliPerera <keshiyaperera1@gmail.com>
2026-06-29 19:18:29 +0100

    Add view list feature

--- main.py
+++ main.py
@@ -6,6 +6,26 @@ def show_menu():
     print("3. Add a Book")
     print("4. View my list")
     print("5. Quit")
+
+movies = []
+def add_movie():
+    title = input("Enter movie title: ")
+    rating = input("Rate it out of 10: ")
+    movie = {"title": title, "rating": rating}
+    movies.append(movie)
+    print(f"'{title}' added to your list!done")
+
+
+def view_list():
+    if len(movies) == 0:
+        print("Your list is empty!")
+    else:
+        print("\nYour movies:")
+        print("---------------------------")
+        for movie in movies:
+            print(f"- {movie['title']} | Rating: {movie['rating']}/10")
+
+
 def main():
     while True:
         show_menu()
@@ -13,12 +33,14 @@ def main():
 
         if choice == "1":
             print ("You choose: Add a movie")
+            add_movie()
         elif choice == "2":
             print("You choose : Add a   TV show")
         elif choice =="3":
             print("You choose: Add a book")
         elif choice =="4":
             print("You choose: View my list")
+            view_list()
         elif choice == "5":
             print("Goodbye!")
             break
